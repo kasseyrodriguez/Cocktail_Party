06ae126be607be7c09c6b75f75941c1a95dc9ccb
Christopher Duenas <chrisduenasonline@gmail.com>
2018-07-19 19:28:14 -0400

    resolve merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_221354) do
+ActiveRecord::Schema.define(version: 2018_07_19_191824) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2018_07_19_221354) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
