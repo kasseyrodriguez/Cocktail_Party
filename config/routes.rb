f7313aaad79eb914328fa2ed3cfe1243263e62d7
kasseyrodriguez <kassseyrodriguez@gmail.com>
2018-07-20 13:41:10 -0400

    Add atribute name to user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#home'
 
-  get 'bartenders', to: 'welcome#bartenders'
-  get 'login', to: 'welcome#login'
-  get 'signup', to: 'welcome#signup'
+
 end
