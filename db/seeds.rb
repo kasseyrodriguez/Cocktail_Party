64129bbbadc7c72f5e812de568479a33bec65738
Karla Cortes <kmcortes7@gmail.com>
2018-07-21 16:53:40 -0400

    Some database changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+15.times do
+  User.create!({
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "123456",
+    password_confirmation: "123456",
+    proper_age: rand(21..40)
+  })
+
+end
