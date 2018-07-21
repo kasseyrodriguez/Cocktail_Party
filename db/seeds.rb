eb88cfbebfd18c179ebd9329ebb94afe406cc25d
Karla Cortes <kmcortes7@gmail.com>
2018-07-21 18:13:42 -0400

    WE DID IT kind of...

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-
 15.times do
   User.create!({
     name: Faker::Name.name,
     email: Faker::Internet.email,
+    bartender: [true, false].sample,
+    gender: ['female', 'male'].sample,
     password: "123456",
     password_confirmation: "123456",
-    proper_age: rand(21..40)
+    proper_age: rand(21..40),
+    home_address: Faker::Address.street_address,
+    flair: nil,
+    mixologist: nil,
+    standard: nil,
   })
+end
 
+if User.where(bartender: true)
+  User.update({
+  rate: rand(10..40),
+  standard: [true, false].sample,
+  flair: [true, false].sample,
+  mixologist: [true, false].sample
+ })
 end
