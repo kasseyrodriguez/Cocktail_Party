9ed732235f37b3e79d69e3aae0871ce9da2227a3
Karla Cortes <kmcortes7@gmail.com>
2018-07-24 17:34:42 -0400

    Individual pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
- has_one_attached :picture
+  has_one_attached :picture
 
 
 end
