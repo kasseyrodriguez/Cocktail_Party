9cc18a29c9baf77c710299f26ef90eb15d6daf1d
Karla Cortes <kmcortes7@gmail.com>
2018-07-25 19:19:06 -0400

    React Changes

--- app/javascript/bundles/App/App.js
+++ app/javascript/bundles/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import NavigationBar from './components/NavigationBar';
-import Jumbotron from './components/Jumbotron'
-
+import Jumbotron from './components/Jumbotron';
+import moment from 'moment';
 class App extends React.Component {
   constructor(){
     super()
@@ -12,9 +12,12 @@ class App extends React.Component {
       results: [],
       bartenderList: [],
       value: "",
-      filters: []
+      filters: [],
+      selectedBartender: {},
+      newDate: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -67,6 +70,24 @@ class App extends React.Component {
     this.setState({ results: results });
   }
 }
+  createEvent = () => {
+    //axios post to evrnt endpoint with sleected bartender, actiually creating the event
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    let eventDate = event.target.value;
+    console.log(eventDate);
+    let dateEntered = new Date(eventDate);
+    // eventDate = moment().format('DD-MMM-YYYY');
+
+    // this.setState({ newDate: eventDate });
+    // console.log(this.state.newDate);
+    // eventDate = moment(eventDate).format("DD-MMM-YYYY");
+    // console.log(eventDate);
+    // this.setState({newDate: eventDate});
+    // console.log(this.state.newDate);
+ }
 
   render() {
     const { results } = this.state;
@@ -88,6 +109,14 @@ class App extends React.Component {
           <input type="radio" value="Male" name="gender" ref="maleBtn"/> Male
           <input type="radio" value="Female" name="gender"/> Female
         </div>
+        <label>Availability:</label>
+          <input type="checkbox" value="monday" name="day"/>Monday
+          <input type="checkbox" value="tuesday" name="day"/>Tuesday
+          <input type="checkbox" value="wednesday" name="day"/>Wednesday
+          <input type="checkbox" value="thursday" name="day"/>Thursday
+          <input type="checkbox" value="friday" name="day"/>Friday
+          <input type="checkbox" value="saturday" name="day"/>Saturday
+          <input type="checkbox" value="sunday" name="day"/>Sunday
         <label>Ratings:</label>
        <select name="rating" onChange={this.handleChange} ref="ratingNumber">
           <option value="5">5</option>
@@ -99,21 +128,23 @@ class App extends React.Component {
         <ul>
           {results.map((result, i) => {
             return (
-                <li key={i}>
-
-                <a href={result.location}>Name: {result.name}
-      Gender: {result.gender}
+                <li key={i}
+                  onClick={ () => {this.setState({selectedBartender: result}) } }>
+                Name: {result.name}
+                Gender: {result.gender}
                 Mixologist: {result.mixologist}
-                  Flair: {result.flair}
-              Standard: {result.standard}
-                  Rating: {result.rating}
-                  Bio: {result.bio}
-                </a>
+                Flair: {result.flair}
+                Standard: {result.standard}
+                Rating: {result.rating}
+                Bio: {result.bio}
+                a>
                 </li>
             );
           })}
         </ul>
         {results.length === 0 && <p>No Results</p>}
+         Date:<div><input type="date"></input></div>
+       <button class="black" onClick={this.handleClick} width="100%"></button>
       </div>
     );
   }
