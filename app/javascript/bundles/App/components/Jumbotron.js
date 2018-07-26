4ee971f70a138044169df236d520d7b3d2873d2e
Karla Cortes <kmcortes7@gmail.com>
2018-07-26 16:15:57 -0400

    Navbar edits

--- app/javascript/bundles/App/components/Jumbotron.js
+++ app/javascript/bundles/App/components/Jumbotron.js
@@ -3,9 +3,8 @@ import React, { Component } from "react";
 class Jumbotron extends Component {
   render() {
     return (
-    <div> Hello World</div>
-      
-
+      <div class="container">
+      </div>
    )
   }
 }
