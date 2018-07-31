0fe677a127b5bfc137b0adbcecf59e4dd821d0ef
Karla Cortes <kmcortes7@gmail.com>
2018-07-30 21:33:03 -0400

    New things

--- app/javascript/bundles/App/components/Search.js
+++ app/javascript/bundles/App/components/Search.js
@@ -5,7 +5,7 @@ import App from '../App';
 const Search = (props) => {
 	return (
 		<div className="jumbotron jumbotron-fluid ">
-			<img src="https://i.imgur.com/0rf0hmD.jpg" alt="logo" width="100%" className="jumbo-img" />
+			<img src="https://i.imgur.com/0rf0hmD.jpg" alt="logo" width="100%" height="30%" className="jumbo-img" />
 			<h1 className="first-header">Choose A Bartender</h1>
 			<h3 className="moto-header">Let Cocktail Party help you find a Bartender for your event!</h3>
 			<input onChange={props.handleSearch} type="search" className="searchbar" placeholder="Search for a Bartender" /> <a href="#"><img src={'http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png'} className="search-icon" /></a>
